8483645bf59757fd2f8bb35bc26f70508a4d5138
khoaht <hotankhoa.qn@gmail.com>
2014-11-27 17:36:57 +0700

    First Commit

---
+++ CoalitionLoyalty/CoalitionLoyalty/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CoalitionLoyalty.MvcApplication" Language="C#" %>
---
+++ CoalitionLoyalty/CoalitionLoyalty/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CoalitionLoyalty.Startup))]
+namespace CoalitionLoyalty
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
